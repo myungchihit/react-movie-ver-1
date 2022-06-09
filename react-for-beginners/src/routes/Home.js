f5a9e71671343637170c1a9aeb9ae8062a921853
JJangGyu <pridom112@naver.com>
2022-06-09 19:53:16 +0900

    add style

--- react-for-beginners/src/routes/Home.js
+++ react-for-beginners/src/routes/Home.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import Movie from "../components/Movie";
+import styles from './Home.module.css';
 
 function Home(){
     const [loading, setLoading] = useState(true);
@@ -29,18 +30,21 @@ function Home(){
     }, [])
  
     return (
-        <div>
+        <div className={styles.container}>
             {loading ? <h1>Loading...</h1> : 
-                <div>{movies.map(movie => 
+                <div className={styles.movies}>
+                    {movies.map(movie => 
                     <Movie 
                         key={movie.id}
                         id={movie.id}
+                        year={movie.year}
                         coverImg={movie.medium_cover_image} 
                         title={movie.title}
                         summary= {movie.summary}
                         genres = {movie.genres}
                         />
-                )}</div>}
+                )}
+                </div>}
         </div>
     ) 
 }
